e0976427c8835ffad267367f19f99e0bd60164cf
Cameron Chin <31452808+cmc6887@users.noreply.github.com>
2022-02-25 19:35:32 -0500

    updates

--- assets/js/valorant.js
+++ assets/js/valorant.js
@@ -4,7 +4,10 @@ function OnHover(x) {
     document.getElementById('ImageContainer').style.visibility = 'visible';     
 }
 function OffHover() {
-    document.getElementById('ImageContainer').style.visibility = 'hidden';
+    
+    setTimeout(function () {
+       //document.getElementById('ImageContainer').style.visibility = 'hidden';
+    }, 5000);
 }
                                 
 
@@ -13,6 +16,9 @@ function OnHover2(x) {
     document.getElementById("MapContainer").src = "../images/Valorant/AgentMaps/"+x+"MapRadar.png";
 }
 function OffHover2() {
-    //document.getElementById("MapContainer").src = "../images/Valorant/ValorantMapRadar.png";
+    setTimeout(function () {
+        document.getElementById("MapContainer").src = "../images/Valorant/AgentMaps/AgentMapRadar.png";
+        }, 3000);
+    
 }
         
